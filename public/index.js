3725535ce3b55b9d69ab5476665e2032d2f029a4
Sarah <vanwars@gmail.com>
2019-03-26 10:45:12 -0500

    bugfix

--- public/index.js
+++ public/index.js
@@ -3,14 +3,11 @@ fetch('/db')
         return response.json();
     })
     .then(data => {
-        //const url = 'https://eecs130-restapi.herokuapp.com/'
-        const url = '/';
         const urls = [];
-        for (key in data) {
-            const count = data[key].length;
-            const endpoint = url + key;
-            urls.push(`<a href="${endpoint}">/${key}</a><sup>${count}x</sup>`);
+        for (endpoint in data) {
+            const count = data[endpoint].length;
+            urls.push(`<a href="/${endpoint}">/${endpoint}</a><sup>${count}x</sup>`);
         }
-        urls.push(`<a href="${url}db">/db</a><sup>state</sup>`);
+        urls.push(`<a href="/db">/db</a><sup>state</sup>`);
         document.getElementById('resources').innerHTML = urls.join('<br>');
     });
