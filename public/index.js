573be8ca192190951e093b3f7001f6717dbe8312
Sarah <vanwars@gmail.com>
2019-03-25 19:40:21 -0500

    fixing typo

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ fetch('http://localhost:3000/db')
     })
     .then(data => {
         //const url = 'https://eecs130-restapi.herokuapp.com/'
-        const url = 'http://localhost:3000/';
+        const url = '/';
         const urls = [];
         for (key in data) {
             const count = data[key].length;
