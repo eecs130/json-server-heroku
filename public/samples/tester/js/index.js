e47931936cdb33df7f47953b7729b370b6f95f41
Sarah <vanwars@gmail.com>
2019-05-19 10:45:35 -0500

    modifying interface

--- public/samples/tester/js/index.js
+++ public/samples/tester/js/index.js
@@ -1,224 +1,205 @@
+let appData = null;
 const rootURL = document.location.protocol + '//' + document.location.host + '/';
-// const routeElement = document.querySelector("#route");
 const menuElement = document.querySelector("#menu");
+const methodElement = document.querySelector("#method");
 const dataContainer = document.querySelector("#data-container");
-const codeContainer = document.querySelector("#code-container");
+const codeContainer = document.querySelector("#code-container code");
+const codeHidden = document.querySelector("input.code");
+const submitElement = document.querySelector("button");
+let successElement = document.querySelector(".alert");
+
 const initialize = () => {
-    // routeElement.value = rootURL + 'comments';
-    // routeElement.focus();
     populateEndpoints();
+
+    // initialize event handlers:
+    submitElement.onclick = go;
+    menuElement.onchange = handleEndpointChange;
+    methodElement.onchange = handleMethodChange;
+    dataContainer.onkeydown = displayCode;
+    dataContainer.onkeyup = displayCode;
+};
+
+const populateMethods = () => {
+    if (getEndpointType() === 'list') {
+        methodElement.innerHTML = `
+            <option value="get">GET</option>
+            <option value="post">POST</option>`;
+    } else {
+        methodElement.innerHTML = `
+            <option value="get">GET</option>
+            <option value="put">PUT</option>
+            <option value="delete">DELETE</option>`;
+    }
+};
+const setAppData = (data) => {
+    appData = data;
+};
+
+const buildEndpointsMenu = () => {
+    menuElement.innerHTML = '';
+    const options = [];
+    for (endpoint in appData) {
+        const label = endpoint.charAt(0).toUpperCase() + endpoint.slice(1)
+        options.push(`<optgroup label="${label}">`);
+        options.push(`<option endpoint-type="list" value="${rootURL}${endpoint}">${rootURL}${endpoint}</option>`);
+        for (item of appData[endpoint]) {
+            options.push(`
+                <option endpoint-type="detail" value="${rootURL}${endpoint}/${item.id}">
+                    ${rootURL}${endpoint}/${item.id}
+                </option>`);
+        }
+        options.push(`</optgroup>`);
+    }
+    menuElement.innerHTML = options.join('');
 };
 
 const populateEndpoints = () => {
     fetch(rootURL + 'db')
         .then(response => response.json())
-        .then(data => {
-            menuElement.innerHTML = '';
-            const options = [];
-            for (endpoint in data) {
-                const label = endpoint.charAt(0).toUpperCase() + endpoint.slice(1)
-                options.push(`<optgroup label="${label}">`);
-                options.push(`<option value="${rootURL}${endpoint}">${rootURL}${endpoint}</option>`);
-                for (item of data[endpoint]) {
-                    options.push(`
-                        <option value="${rootURL}${endpoint}/${item.id}">
-                            ${rootURL}${endpoint}/${item.id}
-                        </option>`);
-                }
-                options.push(`</optgroup>`);
-            }
-            menuElement.innerHTML = options.join('');
-        })
+        .then(setAppData)
+        .then(buildEndpointsMenu)
+        .then(populateMethods)
         .then(issueGetRequest);
 };
+
 const getEndpoint = () => {
     return menuElement.options[menuElement.selectedIndex].value;
 };
+
+const getEndpointType = () => {
+    return menuElement.options[menuElement.selectedIndex].getAttribute('endpoint-type');
+};
+
+const getMethod = () => {
+    return methodElement.options[methodElement.selectedIndex].value;
+};
+
+const handleEndpointChange = () => {
+    // todo: needs to keep state of selected
+    const endpointType = getEndpointType();
+    const method = getMethod();
+    populateMethods();
+    console.log(endpointType);
+    if (method === 'post') {
+        dataContainer.value = '{}';
+    } else {
+        showMessage('Fetching data...');
+        issueGetRequest();
+    }
+};
+
+const handleMethodChange = () => {
+    const method = getMethod();
+    if (method === 'post') {
+        dataContainer.value = '{}';
+    } else {
+        issueGetRequest();
+    }
+    displayCode();
+};
+
 const issueGetRequest = () => {
     // get data from the server:
-    console.log(getEndpoint());
     fetch(getEndpoint())
         .then(response => response.json())
         .then(displayData)
         .then(displayCode)
 };
+
 const displayData = (data) => {
-    dataContainer.innerHTML = JSON.stringify(data, null, 4);
+    console.log(data)
+    if (data) {
+        dataContainer.value = JSON.stringify(data, null, 4);
+    }
 };
 
-const displayCode = () => {
-    codeContainer.innerHTML = `fetch('${getEndpoint()}')
+const generateGetCommand = () => {
+    return `fetch('${getEndpoint()}')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });`
+};
+
+const generateCreateUpdateCommand = (method) => {
+    let formatJSON = dataContainer.value;
+    // format whitespace
+    if (formatJSON !== '{}') {
+        formatJSON = dataContainer.value.replace(/    /g, "            ");
+        formatJSON = formatJSON.replace('}', '        }');
+    }
+    return `fetch('${getEndpoint()}', {
+        method: '${method}',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(${formatJSON})
+    })
     .then(response => response.json())
     .then(data => {
         console.log(data);
     });`;
 };
 
+const generateDeleteCommand = () => {
+    return `fetch('${getEndpoint()}', { 
+        method: 'delete' 
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });`
+};
+
+const showMessage = (message) => {
+    successElement.innerHTML = message;
+    successElement.classList.add('alert-success');
+    var newone = successElement.cloneNode(true);
+    successElement.parentNode.replaceChild(newone, successElement);
+    successElement = newone;
+};
+
+const showConfirmationMessage = () => {
+    const method = getMethod();
+    showMessage(`Your ${method} request was successfully executed.`);
+};
+
+const displayCode = () => {
+    const method = getMethod();
+    console.log(method)
+    if (method === 'get') {
+        codeContainer.innerHTML = codeHidden.value = generateGetCommand();
+    } else if (method === 'post') {
+        codeContainer.innerHTML = codeHidden.value = generateCreateUpdateCommand('post');
+    } else if (method === 'put') {
+        codeContainer.innerHTML = codeHidden.value = generateCreateUpdateCommand('put');
+    } else if (method === 'delete') {
+        codeContainer.innerHTML = codeHidden.value = generateDeleteCommand();
+    }
+    hljs.highlightBlock(codeContainer);
+};
+
+const go = () => {
+    const method = getMethod();
+    let code = codeHidden.value;
+    code = code.replace(/&gt;/g, '>');
+    code = code.replace(/&lt;/g, '<');
+    code = code.slice(0, code.lastIndexOf('});'));
+    code += `
+        displayData(data);
+        displayCode(); 
+    `;
+    if (['delete', 'post'].includes(method)) {
+        code += `
+        populateEndpoints();
+        `;
+    } code += `
+    showConfirmationMessage();
+    `;
+    code += '});'
+    console.log(code);
+    eval(code);
+}
+
 initialize();
-document.querySelector('button').onclick = issueGetRequest;
-menuElement.onchange = issueGetRequest;
-
-
-
-
-
-
-
-
-// const issuePutRequest = (id, data, responseHandler) => {
-//     // update data on the server:
-//     const url = endpoint + '/' + id;
-//     console.log('PUT request:', url, data);
-//     fetch(url, {
-//             method: 'put',
-//             headers: {
-//                 'Accept': 'application/json',
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify(data)
-//         })
-//         .then(response => response.json())
-//         .then(responseHandler);
-// };
-
-// const issueDeleteRequest = (id, responseHandler) => {
-//     // delete data on the server:
-//     const url = endpoint + '/' + id;
-//     console.log('DELETE request:', url);
-//     fetch(url, { method: 'delete' })
-//         .then(responseHandler);
-// };
-
-// const issuePostRequest = (data, responseHandler) => {
-//    // create data on the server:
-//    console.log('POST request:', endpoint, data);
-//    fetch(endpoint, {
-//         method: 'post',
-//         headers: {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//     }).then(responseHandler);
-// };
-
-// const handleUpdate = (ev) => {
-//     const id = ev.target.getAttribute('data-id');
-//     const container = ev.target.parentElement.parentElement;
-//     const data = {
-//         'first_name': container.querySelector('#first_name').value,
-//         'last_name': container.querySelector('#last_name').value
-//     };
-//     const callback = (item) => {
-//         container.innerHTML = getItemHTML(item);
-//         attachEventHandlers();
-//     };
-//     issuePutRequest(id, data, callback)
-// };
-
-// const handleCreate = () => {
-//     const container = document.querySelector('.add-new-panel');
-//     data = {
-//         'first_name': container.querySelector('.first_name').value,
-//         'last_name': container.querySelector('.last_name').value
-//     }
-//     issuePostRequest(data, issueGetRequest);
-//     document.querySelector('.add-new-panel').classList.remove('active');
-// };
-
-// const handleDelete = (ev) => {
-//     const id = ev.target.id;
-//     const areYouSure = confirm('Are you sure that you want to delete item #' + id + '?')
-//     if (!areYouSure) {
-//         return;
-//     }
-//     issueDeleteRequest(id, issueGetRequest);
-// };
-
-// const attachEventHandlers = () => {
-//     for (elem of document.querySelectorAll('.delete')) {
-//         elem.onclick = handleDelete;
-//     }
-//     for (elem of document.querySelectorAll('.edit')) {
-//         elem.onclick = showEditForm;
-//     }
-//     document.querySelector('#add-new').onclick = () => {
-//         const container = document.querySelector('.add-new-panel');
-//         container.querySelector('.first_name').value = '';
-//         container.querySelector('.last_name').value = '';
-//         container.classList.add('active');
-//     };
-//     document.querySelector('.close').onclick = () => {
-//         document.querySelector('.add-new-panel').classList.remove('active');
-//     };
-//     document.querySelector('.add-new-panel .cancel').onclick = () => {
-//         document.querySelector('.add-new-panel').classList.remove('active');
-//     };
-//     document.querySelector('.update.save').onclick = handleCreate;
-    
-// };
-
-// const attachFormEventHandlers = (item, container) => {
-//     container.querySelector('.update').onclick = handleUpdate;
-//     container.querySelector('.cancel').onclick = () => {
-//         container.innerHTML = getItemHTML(item);
-//         attachEventHandlers();
-//     }
-// };
-
-// const showEditForm = (ev) => {
-//     const id = ev.target.id;
-//     const url = endpoint + '/' + id;
-//     const cardElement = ev.target.parentElement.parentElement;
-//     fetch(url)
-//         .then(response => response.json())
-//         .then(item => {
-//             displayForm(item, cardElement);
-//             attachFormEventHandlers(item, cardElement);
-//         });
-//     return false;
-// };
-
-// const displayForm = (item, container) => {
-//     container.innerHTML = `
-//         <div style="margin-top:7px;">
-//             <label>id:</label><span>${item.id}</span><br>
-//             <label>first_name:</label>
-//             <input type="text" id="first_name" value="${item.first_name}"><br>
-            
-//             <label>last_name:</label>
-//             <input type="text" id="last_name" value="${item.last_name}"><br>
-            
-//             <label></label>
-//             <button type="button" data-id="${item.id}" class="update">Update</button>
-//             <button type="button" class="cancel">Cancel</button>
-//         </div>
-//     `;
-// };
-
-// const getItemHTML = (item) => {
-//     return `<div>
-//         <div class="controls">
-//             <i class="fas fa-edit edit" id="${item.id}"></i>
-//             <i class="fas fa-trash delete" id="${item.id}"></i>
-//         </div>
-//         <label>id:</label><span>${item.id}</span><br>
-//         <label>first_name:</label><span>${item.first_name}</span><br>
-//         <label>last_name:</label><span>${item.last_name}</span>
-//     </div>`;
-// };
-
-// const displayItem = (item, container) => {
-//     itemHTML = getItemHTML(item);
-//     container.innerHTML += `
-//         <div class="card-wrapper">${itemHTML}</div>
-//     `;
-// };
-
-// const displayData = (data) => {
-//     const container = document.querySelector("#container");
-//     container.innerHTML = "";
-//     for (item of data) {
-//         displayItem(item, container);
-//     }
-// };
